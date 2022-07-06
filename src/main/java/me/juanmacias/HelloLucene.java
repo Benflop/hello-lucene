8155a70211f1f0a34721f2efac866bbcb78e7c99
Benedict Lee <Benflop@users.noreply.github.com>
2022-07-06 14:26:06 +0800

    Added Code for Crawling & Searching

--- src/main/java/me/juanmacias/HelloLucene.java
+++ src/main/java/me/juanmacias/HelloLucene.java
@@ -3,7 +3,6 @@ package me.juanmacias;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
-import org.apache.lucene.document.StringField;
 import org.apache.lucene.document.TextField;
 import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.IndexReader;
@@ -15,11 +14,50 @@ import org.apache.lucene.search.Query;
 import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.search.TopScoreDocCollector;
 import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.store.RAMDirectory;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.HashSet;
 
 public class HelloLucene {
+    private static HashSet<String> links = new HashSet<String>();
+
+    public HelloLucene() {
+        links = new HashSet<String>();
+    }
+
+    public static void getPageLinks(String URL, IndexWriter w, int num) throws IOException {
+        //4. Check if you have already crawled the URLs
+        //(we are intentionally not checking for duplicate content in this example)
+        if (num != 0) {
+
+            if (!links.contains(URL)) {
+                try {
+                    //4. (i) If not add it to the index
+
+                    //2. Fetch the HTML code
+                    org.jsoup.nodes.Document document = Jsoup.connect(URL).get();
+                    //3. Parse the HTML to extract links to other URLs
+                    Elements linksOnPage = document.select("a[href]");
+                    addDoc(w, document.title(), URL);
+                    System.out.println("Num:" + num + " Title: " + document.title() + " Url: " + URL);
+                    //5. For each extracted URL... go back to Step 4.
+
+                    for (Element page : linksOnPage) {
+                        getPageLinks(page.attr("abs:href"), w, num - 1);
+                    }
+
+                } catch (IOException e) {
+                    System.err.println("For '" + URL + "': " + e.getMessage());
+                }
+            }
+        }
+    }
 
     public static void main(String[] args) throws IOException {
         // 0. Specify the analyzer for tokenizing text.
@@ -27,19 +65,15 @@ public class HelloLucene {
         StandardAnalyzer analyzer = new StandardAnalyzer();
 
         // 1. create the index
-        Directory index = new RAMDirectory();
-
+        // Directory index = new RAMDirectory();
+        FSDirectory index = FSDirectory.open(Paths.get("index.lucene"));
         IndexWriterConfig config = new IndexWriterConfig(analyzer);
 
         IndexWriter w = new IndexWriter(index, config);
-        addDoc(w, "Lucene in Action", "193398817");
-        addDoc(w, "Lucene for Dummies", "55320055Z");
-        addDoc(w, "Managing Gigabytes", "55063554A");
-        addDoc(w, "The Art of Computer Science", "9900333X");
+        getPageLinks("https://en.wikipedia.org/wiki/List_of_universities_in_the_United_Kingdom", w, 2);
         w.close();
-
         // 2. query
-        String queryString = args.length > 0 ? args[0] : "9900333X";
+        String queryString = args.length > 0 ? args[0] : "University";
 
         // the "title" arg specifies the default field to use
         // when no field is explicitly specified in the query.
@@ -47,6 +81,7 @@ public class HelloLucene {
         try {
             String[] fields = {"isbn", "title"};
             query = new MultiFieldQueryParser(fields, analyzer).parse(queryString);
+
         } catch (org.apache.lucene.queryparser.classic.ParseException e) {
             e.printStackTrace();
         }
@@ -61,23 +96,35 @@ public class HelloLucene {
 
         // 4. display results
         System.out.println("Found " + hits.length + " hits.");
+
         for (int i = 0; i < hits.length; ++i) {
+
             int docId = hits[i].doc;
             Document d = searcher.doc(docId);
-            System.out.println((i + 1) + ". " + d.get("isbn") + "\t" + d.get("title"));
+            System.out.println((i + 1) + ". " + d.get("title") + "\t" + d.get("url"));
         }
-
         // reader can only be closed when there
         // is no need to access the documents anymore.
         reader.close();
+        //testCrawl(); Single Crawl
     }
 
-    private static void addDoc(IndexWriter w, String title, String isbn) throws IOException {
+    private static void testCrawl() throws IOException {
+
+        org.jsoup.nodes.Document doc = Jsoup.connect("https://en.wikipedia.org/wiki/University").get();
+        doc.select("title").forEach(System.out::println);
+        // doc.select(".mw-parser-output p a").forEach(System.out::println);
+        doc.select("a").forEach(System.out::println);
+
+
+    }
+
+    private static void addDoc(IndexWriter w, String title, String url) throws IOException {
         Document doc = new Document();
         doc.add(new TextField("title", title, Field.Store.YES));
 
         // use a string field for isbn because we don't want it tokenized
-        doc.add(new StringField("isbn", isbn, Field.Store.YES));
+        doc.add(new TextField("url", url, Field.Store.YES));
         w.addDocument(doc);
     }
 }
